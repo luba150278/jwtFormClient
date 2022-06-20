886f6eef7ec6a4f5ce6c2b5d3770c7741dfb9068
Lyubov Myetolkina <luba150278@gmail.com>
2022-06-20 20:40:34 +0300

    headers config

--- src/http/header-config.ts
+++ src/http/header-config.ts
@@ -1,3 +1,5 @@
+import { AxiosRequestHeaders } from 'axios';
+
 export const headersConfig: AxiosRequestHeaders = {
   'Content-Type': 'application/json',
   Authorization: `Bearer ${localStorage.getItem('token')}`,
